170a9f6dcaaf6f4c7e5b803d289aa16d3503a6c4
szendi_attila <szendiattila@gmail.com>
2021-01-08 14:22:32 +0100

    remove LocalScraperService.php

--- app/Services/UnavailableScraperService.php
+++ app/Services/UnavailableScraperService.php
@@ -6,13 +6,7 @@ use Psr\Http\Message\ResponseInterface;
 
 class UnavailableScraperService extends GoutteScraperService
 {
-    const MOCK_FILE = "sainsburys-mock.html";
     const NOT_EXISTING_URI = "http://not-exists.hu/jatekok";
-
-    public function getUrl(): string
-    {
-        return asset(static::MOCK_FILE);
-    }
 
     public function callApi(string $productSlug): ResponseInterface
     {
--- tests/Unit/ScrapeServiceTest.php
+++ tests/Unit/ScrapeServiceTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit;
 
-use App\Services\LocalScraperService;
+use App\Services\GoutteScraperService;
 use App\Services\UnavailableScraperService;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ConnectException;
@@ -29,7 +29,7 @@ class ScrapeServiceTest extends TestCase
      * @throws GuzzleException
      * @test
      */
-    public function it_throws_exception_if_the_SainsBurys_api_service_is_down()
+    public function it_throws_exception_if_the_sainsburys_api_service_is_down()
     {
         $this->expectException(ConnectException::class);
         $class = new UnavailableScraperService($this->guzzle);
@@ -42,7 +42,7 @@ class ScrapeServiceTest extends TestCase
      */
     public function it_returns_the_products_array()
     {
-        $class = new LocalScraperService($this->guzzle);
+        $class = new GoutteScraperService($this->guzzle);
         $result = $class->scrape();
         $this->assertIsArray($result);
         $this->assertArrayHasKey('title', $result[0]);
